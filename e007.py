1293576e271b854ed58c639b5765b4be9e218a1b
ilya-il <il.khimki@yandex.ru>
2017-11-15 23:06:55 +0300

    p7 in 0.06sec

--- e007.py
+++ e007.py
@@ -9,6 +9,8 @@
 
 __author__ = 'ilya_il'
 
+import time
+
 
 def get_prime_numbers(upper_bound):
     prime_numbers = [2, ]
@@ -40,5 +42,39 @@ def get_prime_number_by_pos(pos):
     return prime_numbers[-1]
 
 
-print(get_prime_number_by_pos(10001))
+def get_prime_number_by_pos2(pos):
+    upper_bound = 105000
+    nums = [n for n in range(2, upper_bound)]
 
+    # get prime numbers
+    n = 0
+    while nums[n]**2 <= upper_bound:
+        if nums[n] != 0:
+            # n - index of prime number
+            # pn - prime number
+            pn = nums[n]
+
+            for i in range(pn + n, upper_bound - 2, pn):
+                nums[i] = 0
+
+        n += 1
+
+    # count prime numbers
+    n = 0
+    res = 0
+    print(nums)
+    for i in range(0, upper_bound - 2):
+        if nums[i] != 0:
+            n += 1
+        if n == pos:
+            res = nums[i]
+            print(pos)
+            break
+
+    # return last number in list
+    return res
+
+
+st = time.time()
+print(get_prime_number_by_pos2(10001))
+print("--- %s seconds ---" % (time.time() - st))
