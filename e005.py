70828efeac8d4a314e0e1ac3d569ab646992ee5e
ilya-il <il.khimki@yandex.ru>
2017-11-16 18:21:35 +0300

    work for 10, doesn't work for 20

--- e005.py
+++ e005.py
@@ -11,10 +11,28 @@
 
 __author__ = 'ilya_il'
 
-n = 2520/2
 
-print(n)
-for i in range(1, 11):
-    print(n % i)
+def get_factors(num):
+    fact = list()
+    for i in range(2, int(num/2) + 1):
+        if num % i == 0:
+            fact.append(i)
+
+    # fact.append(num)
+    return fact
 
 
+def get_number(init):
+    res = 1
+    nums = [x for x in range(init, 0, -1)]
+    # nums = [x for x in range(1, init + 1)]
+    for n in nums:
+        f = get_factors(n)
+        if n != 3 and len(f) < 2:
+            res *= n
+        print('{p1} - {p2}'.format(p1=n, p2=get_factors(n)))
+    print(nums)
+    print(res)
+
+
+get_number(20)
