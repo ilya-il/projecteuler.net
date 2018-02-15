0cf00ceadeab332d7a6dd497462bdc4541c5ae48
ilya-il <il.khimki@yandex.ru>
2018-02-15 15:25:47 +0300

    p102 solved

--- p102.py
+++ p102.py
@@ -8,30 +8,60 @@
 
 __author__ = 'ilya_il'
 
+import math
+import os
 
-def check_line(d1, d2, d3):
+
+def get_len(d1, d2):
+    """Get length"""
+    return math.sqrt((d2[0] - d1[0])**2 + (d2[1] - d1[1])**2)
+
+
+def get_square(a, b, c):
+    # Geron's formula of triangle square
+    # S = sqrt(p*(p-a)*(p-b)*(p-c))
+    p = (a + b + c)/2
+
+    return math.sqrt(p * (p - a) * (p - b) * (p - c))
+
+
+def check0(d1, d2, d3):
     """
     :param d1: (x1, y1)
     :param d2: (x2, y2)
     :param d3: (x3, y3)
     :return: True/False
     """
-    #
-    # https://ru.wikipedia.org/wiki/Прямая
-    # x = x1 + (x2-x1)*(y-y1)/(y2-y1)
-    # y = y1 + (x-x1)*(y2-y1)/(x2-x1)
-    #
-    # (0, n) - line crosses Y-line
-    # (m, 0) - line crosses X-line
-    #
-    # m = x1 + (x2-x1)*(0-y1)/(y2-y1)
-    # n = y1 + (0-x1)*(y2-y1)/(x2-x1)
+    a = get_len(d1, d2)
+    b = get_len(d2, d3)
+    c = get_len(d1, d3)
 
-    return True
+    fs = get_square(a, b, c)
+    s1 = get_square(get_len((0, 0), d2), get_len(d2, d3), get_len((0, 0), d3))
+    s2 = get_square(get_len(d1, (0, 0)), get_len((0, 0), d3), get_len(d1, d3))
+    s3 = get_square(get_len(d1, d2), get_len(d2, (0, 0)), get_len(d1, (0, 0)))
+
+    if math.fabs(fs - s1 - s2 - s3) < 0.0005:
+        return True
+    else:
+        return False
 
 
 def solve_problem():
-    pass
+    # print(check0((-340, 495), (-153, -910), (835, -947)))
+    # print(check0((-175, 41), (-421, -714), (574, -645)))
+    ih = open(os.path.join(os.path.dirname(os.path.abspath(__file__)), os.path.join('res', 'p102_triangles.txt')))
+    data = ih.readlines()
+    ih.close()
+
+    res = 0
+    for d in data:
+        dots = [int(x) for x in d.strip('\n').split(',')]
+
+        if check0((dots[0], dots[1]), (dots[2], dots[3]), (dots[4], dots[5])):
+            res += 1
+
+    print(res)
 
 
 solve_problem()
